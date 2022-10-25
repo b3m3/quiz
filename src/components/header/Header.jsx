2f3690f8512934dd5018ed2963dcc7a878545475
b3m3 <uwm.d.c.a@gmail.com>
2022-10-25 17:09:48 +0300

    add Header images

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,22 @@
-import brain from './img/brain.png';
+import General from './img/general.png';
+import Geography from './img/geography.png';
+import Sport from './img/sport.png';
+import Society from './img/society.png';
 
 import style from './header.module.css';
 
-const Header = () => {
+const Header = ({ title }) => {
+
   return (
     <div className="container">
       <header className={style.header}>
 
-        <h1>Quiz</h1>
+        <h1>{title ? title.category : 'Quiz'}</h1>
 
         <div className={style.image}>
           <img 
-            src={brain}
-            alt="Quiz"
+            src={title ? `${title.category.split(' ')[0]}` : General}
+            alt="Image"
           />
         </div>
       </header>
