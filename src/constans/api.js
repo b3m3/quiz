438a95bb094bee97ee715ea4ea7bd8fe0e56b5ff
b3m3 <uwm.d.c.a@gmail.com>
2022-11-03 10:05:20 +0200

    remove Start

--- src/constans/api.js
+++ src/constans/api.js
@@ -1,8 +1,15 @@
-const ROOT_QUIZ = 'https://the-trivia-api.com/api/';
+export const ROOT_QUIZ = 'https://the-trivia-api.com/api/';
 
-const CATEGORIES = 'categories';
-const METADATA = 'metadata';
-const TAGS = 'tags'
+export const CATEGORY = 'categories';
+export const METADATA = 'metadata';
+export const TAG = 'tags';
 
-export const CATEGORIES_URL = ROOT_QUIZ+CATEGORIES;
-export const TAGS_URL = ROOT_QUIZ+TAGS;
+export const QUESTIONS = 'questions?';
+export const LIMIT = 'limit=';
+export const DIFFICULTY = 'difficulty=';
+export const TAGS = 'tags=';
+export const CATEGORIES = 'categories=';
+
+export const DEFAULT_URL = ROOT_QUIZ+QUESTIONS;
+export const CATEGORIES_URL = ROOT_QUIZ+CATEGORY;
+export const TAGS_URL = ROOT_QUIZ+TAG;
--- src/containers/app/App.jsx
+++ src/containers/app/App.jsx
@@ -1,14 +1,22 @@
-import Header from '../../components/header';
-import Start from '../start/Start';
+import { useState } from 'react';
+
+import StartPage from '../startPage/StartPage';
+import QuizPage from '../quizPage/QuizPage';
+import TotalPage from '../totalPage/TotalPage';
 
 import './app.css';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const pages = [StartPage, QuizPage, TotalPage];
 
   return (
     <div className="app">
-      <Header />
-      <Start />
+      {pages.map((Page, index) => (
+        <div className='app-wrapp' key={index}>
+          {currentPage === index && <Page setCurrentPage={setCurrentPage}/>} 
+        </div>
+      ))}
     </div>
   );
 }
