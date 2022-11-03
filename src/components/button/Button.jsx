6c4d3c6be5cc38377327ce99d1ce19cb016758d5
b3m3 <uwm.d.c.a@gmail.com>
2022-11-03 10:01:11 +0200

    Button onClick func

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,9 +1,31 @@
 import style from './button.module.css';
 
-const Button = ({ name }) => {
+const Button = ({ name, numberOfQuestions, currentQuestion, setCurrentPage, setCurrentQuestion }) => {
+
+  const onClick = () => {
+    if (setCurrentPage && name === "Start") {
+      return setCurrentPage(1);
+    }
+
+    if (currentQuestion >= (numberOfQuestions - 1)) {
+      return setCurrentPage(2);
+    }
+    
+    if (setCurrentQuestion && currentQuestion <= (numberOfQuestions - 1)) {
+      return setCurrentQuestion(c => c + 1);
+    }
+
+    return;
+  }
+
   return (
-    <div className={style.wrapp}>
-      <button>
+    <div 
+      className={style.wrapp}
+      style={name === 'Start' ? {padding: '50px 0 30px 0'} : null}
+    >
+      <button 
+        onClick={() => onClick()}
+      >
         {name}
       </button>
     </div>
