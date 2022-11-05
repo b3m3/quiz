108df9bf2a9fcaa993c63824a0629dadf16ef935
b3m3 <uwm.d.c.a@gmail.com>
2022-11-05 10:38:48 +0200

    TotalPage Counter result

--- src/containers/totalPage/TotalPage.jsx
+++ src/containers/totalPage/TotalPage.jsx
@@ -1,16 +1,29 @@
+import { useContext } from 'react';
+
 import Header from '../../components/header/Header';
 import Counter from '../../components/counter/Counter';
 import Button from '../../components/button/Button';
 
+import { Context } from '../../context/context';
+
 import style from './total.module.css';
 
-const TotalPage = () => {
+const TotalPage = ({ setCurrentPage }) => {
+  const {correctAnswers, rangevalue} = useContext(Context);
+
   return (
     <div className="container">
       <div className={style.total}>
-        <Header total/>
-        <Counter big />
-        <Button name={'Start over'} />
+        <Header total />
+        <Counter 
+          big
+          guessedQuestions={correctAnswers - 1}
+          totalQuestions={rangevalue}
+        />
+        <Button 
+          name={'Start over'}
+          setCurrentPage={setCurrentPage}
+        />
       </div>
     </div>
   );
